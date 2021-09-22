1c6464863913d26a9a082c0b3b6ab8ec9b284d54
Duong <maihoangduong11062000@gmail.com>
2021-09-22 17:13:44 +0700

    add-gitignore-and-read-auth-file-in-contextListener

--- src/java/fptu/swp/listener/LoadFileServletListener.java
+++ src/java/fptu/swp/listener/LoadFileServletListener.java
@@ -9,8 +9,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -24,16 +27,21 @@ public class LoadFileServletListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        loadFileAuth(sce);
+        loadFileRoadMap(sce);
+    }
+    
+    private void loadFileRoadMap(ServletContextEvent sce) {
         Map<String, String> roadmap = new HashMap<>();
 
         ServletContext context = sce.getServletContext();
         String realPath = context.getRealPath("/") + "WEB-INF\\" + context.getInitParameter("ROAD_MAP_TXT_FILE_NAME");
-        
+
         FileReader fr = null;
         BufferedReader bf = null;
         String line;
         final String TOKEN_SPLIT = context.getInitParameter("TOKEN_SPLIT");
-        
+
         try {
             File f = new File(realPath);
             if (!f.exists()) {
@@ -41,14 +49,14 @@ public class LoadFileServletListener implements ServletContextListener {
             }
             fr = new FileReader(f);
             bf = new BufferedReader(fr);
-            
+
             // read each line and load to map 
             while ((line = bf.readLine()) != null) {
                 int IndexOfSeparator = line.indexOf(TOKEN_SPLIT);
-                
+
                 String name = line.substring(0, IndexOfSeparator);
                 String value = line.substring(IndexOfSeparator + 1);
-                
+
                 // add into RoadMap
                 roadmap.put(name, value);
             }
@@ -72,13 +80,71 @@ public class LoadFileServletListener implements ServletContextListener {
                 ex.printStackTrace();
             }
         }
+
     }
+
+    private void loadFileAuth(ServletContextEvent sce) {
+        Map<String, Set<String>> authMap = new HashMap<>();
+        Set<String> setOfServletPermission;
+        
+        ServletContext context = sce.getServletContext();
+        String realPath = context.getRealPath("/") + "WEB-INF\\" + context.getInitParameter("AUTH_FILE_NAME");
+
+        FileReader fr = null;
+        BufferedReader bf = null;
+        String line;
+        final String TOKEN_SPLIT = context.getInitParameter("TOKEN_SPLIT");
+
+        try {
+            File f = new File(realPath);
+            if (!f.exists()) {
+                return;
+            }
+            fr = new FileReader(f);
+            bf = new BufferedReader(fr);
+
+            // read each line and load to map 
+            while ((line = bf.readLine()) != null) {
+                int IndexOfSeparator = line.indexOf(TOKEN_SPLIT);
+
+                String name = line.substring(0, IndexOfSeparator);
+                
+                String strOfPermission = line.substring(IndexOfSeparator + 1);
+                setOfServletPermission = new HashSet<>(Arrays.asList(strOfPermission.split(",")));
+                
+                // add into RoadMap
+                authMap.put(name, setOfServletPermission);
+            }
+            context.setAttribute("AUTH", authMap);
+
+            authMap.keySet().forEach((key) -> {
+                System.out.println(key + ":" + authMap.get(key));
+            });
+
+        } catch (NullPointerException | UnsupportedOperationException | IOException npe) {
+            npe.printStackTrace();
+        } finally {
+            try {
+                if (fr != null) {
+                    fr.close();
+                }
+                if (bf != null) {
+                    bf.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         try {
             ServletContext context = sce.getServletContext();
-            context.removeAttribute(context.getInitParameter("ROAD_MAP_ATTRIBUTE_NAME"));
+            context.removeAttribute("ROADMAP");
+            context.removeAttribute("AUTH");
         } catch (UnsupportedOperationException ex) {
             ex.printStackTrace();
         }
